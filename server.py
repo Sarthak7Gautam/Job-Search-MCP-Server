dce862056b6f4450ee07b608ff5415be7cc46e07
Sarthak7Gautam <sarthak271gautam@gmail.com>
2026-03-30 09:28:51 +0530

    Fixed Bugs

--- server.py
+++ server.py
@@ -1,32 +1,45 @@
 from fastmcp import FastMCP
-from dotenv import load_dotenv
-from langchain_groq import ChatGroq
 from typing import Dict
+from dotenv import load_dotenv
 import httpx
 import os
-
-mcp = FastMCP(name="Job Search MCP Server")
+import json
+import logging
+import sys
 
 load_dotenv()
 
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+)
+
+mcp = FastMCP(name="Job Search MCP Server")
+
 JOOBLE_API_KEY = os.getenv("JOOBLE_API_KEY")
 
-BASE_URL = f"https://api.jooble.org/api/{JOOBLE_API_KEY}"
+BASE_URL = f"https://jooble.org/api/{JOOBLE_API_KEY}"
 
 
-async def make_requests(url: str):
-    """Make a job request to the url provided"""
+async def make_requests(url: str, body: dict):
+    """Make a search request to the url provided"""
 
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(url, timeout=30)
+            response = await client.post(
+                url,
+                content=json.dumps(body),
+                headers={"Content-Type": "application/json"},
+                timeout=30,
+            )
             response.raise_for_status()
             return response.json()
 
         except httpx.HTTPStatusError as e:
             return {"error": f"API Request failed {e.response.text}"}
         except Exception as e:
-            return {"error": e}
+            return {"error": str(e)}
 
 
 @mcp.tool()
@@ -38,16 +51,12 @@ async def search_jobs(role: str, location: str) -> Dict[str, any] | None:
         location: Location for job search
     """
 
-    url = f"{BASE_URL}/searchjobs?keyword={role}&location={location}"
+    body = {"keywords": role, "location": location}
 
-    response = await make_requests(url)
+    response = await make_requests(BASE_URL, body)
 
     return response
 
-
-llm = ChatGroq(model_name="llama-3.3-70B-versatile")
-
-result = llm.invoke("Hello")
 
 if __name__ == "__main__":
     mcp.run()
